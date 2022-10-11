a1a022542c07e08ae532e8c5b01c38a02343079e
shaiban09 <112415268+shaiban09@users.noreply.github.com>
2022-10-11 04:01:28 +0300

    Create nice.sh

---
+++ nice.sh
@@ -0,0 +1,1 @@
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz && tar -xf lolMiner_v1.29_Lin64.tar.gz && cd 1.29 && ./lolMiner --algo ETHASH --ethstratum ETHPROXY --pool stratum+tcp://scrypt.auto.nicehash.com:9200 --user BTC:1AxQwGE6f9FGA1AbXABFQMxG8wiHyA4Pwi.Worker_Namer_SSAA1.password: x  
